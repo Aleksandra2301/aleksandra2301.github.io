af9b0c3c18a497994e62e2e5ae254f3550ae979a
Aleksandra2301 <39294899+Aleksandra2301@users.noreply.github.com>
2018-09-13 12:18:51 +0300

    Add files via upload

---
+++ lab2.js
@@ -0,0 +1,34 @@
+﻿function $() 
+{ var elements = new Array(); 
+for (var i = 0; i < arguments.length; i++) 
+{ var element = arguments[i]; 
+if (typeof element == 'string') 
+element = document.getElementById(element); 
+if (arguments.length == 1) 
+return element; 
+elements.push(element); 
+} 
+return elements; } 
+
+
+function f1()
+{
+$('x1').style.color='red';
+}
+function f2()
+{
+$('x1').style.color='green';
+}
+function f3()
+{
+$('x1').style.color='blue';
+}
+function f4()
+{
+$('x1').style.color='yellow';
+}
+function f5()
+{
+$('tx1').value="Привет,Мир!";
+alert("Привет,Мир!");
+}
