cdcf0a22da477da723bc8bf3028d1160fd667ca4
Aleksandra2301 <39294899+Aleksandra2301@users.noreply.github.com>
2018-09-20 12:19:55 +0300

    Update lab2b.js

--- lab2b.js
+++ lab2b.js
@@ -1,15 +1,3 @@
-﻿function $() 
-{ var elements = new Array(); 
-for (var i = 0; i < arguments.length; i++) 
-{ var element = arguments[i]; 
-if (typeof element == 'string') 
-element = document.getElementById(element); 
-if (arguments.length == 1) 
-return element; 
-elements.push(element); 
-} 
-return elements; } 
-
 function f11()
 {
 $('p1').src="чирва.png";
