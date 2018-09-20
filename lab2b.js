71632617f32ff3694becc184752a3decf3f03e86
Aleksandra2301 <39294899+Aleksandra2301@users.noreply.github.com>
2018-09-20 12:00:25 +0300

    Add files via upload

---
+++ lab2b.js
@@ -0,0 +1,32 @@
+﻿function $() 
+{ var elements = new Array(); 
+for (var i = 0; i < arguments.length; i++) 
+{ var element = arguments[i]; 
+if (typeof element == 'string') 
+element = document.getElementById(element); 
+if (arguments.length == 1) 
+return element; 
+elements.push(element); 
+} 
+return elements; } 
+
+function f11()
+{
+$('p1').src="чирва.png";
+}
+function f22()
+{
+$('p1').src="бубна.png";
+}
+function f33()
+{
+$('p1').src="трефа.png";
+}
+function f44()
+{
+$('p1').src="пика.png";
+}
+function rr()
+{
+$('area1').value="Привет, Мир!!!";
+}
