b1190d25baac1c5c03e29b70d56054d4a4cb23c6
Aleksandra2301 <39294899+Aleksandra2301@users.noreply.github.com>
2018-09-27 22:18:32 +0300

    Add files via upload

---
+++ lab3_2.js
@@ -0,0 +1,24 @@
+﻿function $() 
+{ var elements = new Array(); 
+for (var i = 0; i < arguments.length; i++) 
+{ var element = arguments[i]; 
+if (typeof element == 'string') 
+element = document.getElementById(element); 
+if (arguments.length == 1) 
+return element; 
+elements.push(element); 
+} 
+return elements; } 
+
+function f1()
+{
+var a = parseFloat($('tx11').value);
+var b = parseFloat($('tx22').value);
+var c = parseFloat($('tx33').value);
+min=a;
+if (min > b)
+min=b;
+if (min > c)
+min=c;
+$('tx9').value=min;
+}
