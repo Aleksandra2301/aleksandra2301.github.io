b3ee056203b84d41fd195eb0ab931f5579122a2f
Aleksandra2301 <39294899+Aleksandra2301@users.noreply.github.com>
2018-10-11 12:25:14 +0300

    Add files via upload

---
+++ lab3_1cikly2.js
@@ -0,0 +1,27 @@
+﻿function $() 
+{ var elements = new Array(); 
+for (var i = 0; i < arguments.length; i++) 
+{ var element = arguments[i]; 
+if (typeof element == 'string') 
+element = document.getElementById(element); 
+if (arguments.length == 1) 
+return element; 
+elements.push(element); 
+} 
+return elements; } 
+
+function f11()
+{
+N=parseInt($('tx11').value);
+A0=2;
+$('tx44').value='A0='+A0+'; ';
+AP=2;
+for( k = 1; k <= N; k++)
+{
+AN=A0+1/AP;
+
+$('tx44').value=$('tx44').value+'A'+k+'='+AN+'; ';
+AP=AN
+}
+}
+
