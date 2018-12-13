b30eb9e069891ef91fc7a95e0f0d3ab17fa8ee06
Aleksandra2301 <39294899+Aleksandra2301@users.noreply.github.com>
2018-12-13 11:34:45 +0200

    Update array.js

--- array.js
+++ array.js
@@ -10,34 +10,3 @@ elements.push(element);
 } 
 return elements; }
 
-function f(x)
-{
-var st = "1234567890.";
-var mass = x.split('.');
-f = "True";
-if(mass.length==3)
-{
-for(i=0;i<x.length;i++)
-{
-if(st.indexOf(x.charAt(i))<0)
-f="False";
-}
-}
-else
-f="False";
-return f;
-
-}
-/*
-var d = Date.parse(x.replace( /[.]/g,"-"));	
-if( isNaN(d) )
-{
-return false;
-}
-else
-{
- return true;
-}
-/*
-		
-}
