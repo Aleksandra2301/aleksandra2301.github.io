737fe15b62b38bb2201c4f0250c6d72ca2305452
Aleksandra2301 <39294899+Aleksandra2301@users.noreply.github.com>
2018-12-26 09:56:03 +0200

    Update array.js

--- array.js
+++ array.js
@@ -9,4 +9,29 @@ return element;
 elements.push(element); 
 } 
 return elements; }
+function f(x)
+{
+
+var str = "1234567890.";
+var mass = x.split('.');
+
+f1="True";
+if(mass.length!=3)
+f1="False";
+
+for(i=0;i<x.length;i++)
+{
+tt=x.charAt(i);
+if(str.indexOf(tt)==-1)
+f1="False";
+}
+mass[0]
+if(parseInt(mass[0])>31)
+f1="False";
+mass[1]
+if(parseInt(mass[1])>12)
+f1="False";
+
+return f1;
+}
 
